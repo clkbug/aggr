07eb4b8992ddfe2404d0bd462e2ca67082509560
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2020-05-29 05:22:47 +0900

    適当に実装

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello")
+	n := flag.Int("n", 1, "n: the number of rows")
+	sep := flag.String("d", "\t", "d: separator of output")
+	flag.Parse()
+
+	s := bufio.NewScanner(os.Stdin)
+	buf := make([][]string, *n)
+
+	for i := 0; s.Scan(); i = (i + 1) % *n {
+		buf[i] = append(buf[i], s.Text())
+	}
+
+	for _, x := range buf {
+		fmt.Println(strings.Join(x, *sep))
+	}
+
 }
